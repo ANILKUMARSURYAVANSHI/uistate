d389cd59518e1af9cd98a64958236b3911909f6f
ANIL KUMAR SURYAVANSHI <thakurinfotech@gmail.com>
2016-07-28 16:11:13 +0530

    new added url router otherwise
    make code run

--- javascripts/application.js
+++ javascripts/application.js
@@ -1,13 +1,21 @@
 'use strict';
 var app = angular.module('testui', ['ui.router']);  //'ui.router'
-app.config(function($stateProvider, $urlRouterProvider) {   
-	 $stateProvider.state('state1',{
+app.config(function($stateProvider, $urlRouterProvider) { 
+
+  	$urlRouterProvider.otherwise('/');
+	 $stateProvider
+	 .state('state1',{
 	 	url: "/",
 	 	templateUrl : "meraview.html"
 	 })
-})
+	  .state('state2',{
+	 	url: "/anil",
+	 	template : "<h6>w r uuu</h6>"
+	 });
+});
 app.controller('anil', ['$scope', function($scope){
 
 	$scope.rt = "kumars";
 	
-}])
+}]);
+
